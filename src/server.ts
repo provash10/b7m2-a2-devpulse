83a8e954621d3c6176b291075f8463366920e28c
Provash <provash20cb@gmail.com>
2026-05-24 12:10:26 +0600

    Postman POST- Testing by app.use middleware with express

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express, { type Application, type Request, type Response } from "express"
+const app:Application = express();
+const port = 5000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res:Response) => {
+  // res.send('Dev Pulse Server Running !')
+  res.status(200).json({
+    message:"Express Server",
+    "author": "Dev Pulse",
+  })
+})
+
+//POST
+app.post('/',async(req: Request, res: Response)=>{
+  console.log(req.body);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
